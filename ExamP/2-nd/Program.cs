f297a87028f32a174791a9549d95f93cc672d79b
Kalito98 <Kaloyan.Yanev98@gmail.com>
2016-04-20 16:15:52 +0300

    ExamP 20.04.2016

---
+++ ExamP/2-nd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //read input
+        int module = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        long result = 0;
+        text = text.ToLower();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            char ch = text[i];
+            if (ch == '@')
+            {
+                break;
+            }
+            else if (48 <= ch && ch <= 57) // ('0' <= ch && ch <= '9')
+            {
+                result *= (ch - '0');
+            }
+            else if ('a' <= ch && ch <= 'z')
+            {
+                result += (ch - 'a');
+            }
+            else
+            {
+                result %= module;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
+
