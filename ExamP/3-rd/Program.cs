92f0088faf3e7686d50a8dbe0cc15ea8b2ea27c4
Kalito98 <Kaloyan.Yanev98@gmail.com>
2016-04-20 17:17:18 +0300

    add new problem

--- ExamP/3-rd/Program.cs
+++ ExamP/3-rd/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Numerics;
 
 class Program
 {
     static void Main()
     {
-
+        string input = "123456789"; // Console.ReadLine();
+        BigInteger product = 1;
+        while (input.Length > 0)
+        {
+            input = input.Substring(0, input.Length - 1);
+            int sum = 0;
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                sum += input[i] - '0';
+            }
+            product *= sum;
+        }
     }
 }
 
