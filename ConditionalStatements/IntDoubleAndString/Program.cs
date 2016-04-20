5a5ba645185b36d00db1b77e208f84a97cdebcfa
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-04-20 22:22:22 +0300

    Added some homework

---
+++ ConditionalStatements/IntDoubleAndString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string type = Console.ReadLine();
+
+        if (type == "integer")
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number + 1);
+        }
+        else if (type == "real")
+        {
+            double number2 = double.Parse(Console.ReadLine());
+            number2 += 1;
+            Console.WriteLine("{0:F2}", number2);
+
+        }
+        else if (type == "text")
+        {
+            string number3 = Console.ReadLine();
+            Console.WriteLine(number3 + "*");
+        }
+    }
+}
+
--- ConditionalStatements/Sort3Numbers/Program.cs
+++ ConditionalStatements/Sort3Numbers/Program.cs
@@ -8,18 +8,56 @@ class Program
         float b = float.Parse(Console.ReadLine());
         float c = float.Parse(Console.ReadLine());
 
-        if (a < b)
+        if (a > b && a > c)
         {
+            Console.Write(a + " ");
             if (b > c)
             {
-                Console.WriteLine("{0} {1} {2}", a, b, c);
+                Console.Write(b + " ");
+                Console.Write(c + " ");
             }
-            else 
+            else
             {
+                Console.Write(c + " ");
+                Console.Write(b + " ");
+
+            }
+        }
+        if (b > a && b > c)
+        {
+            Console.Write(b + " ");
+
+            if (a > c)
+            {
+                Console.Write(a + " ");
+                Console.Write(c + " ");
+
+            }
+            else
+            {
+                Console.Write(c + " ");
+                Console.Write(a + " ");
 
             }
 
         }
+        if (c > a && c > b)
+        {
+            Console.Write(c + " ");
+
+            if (b > a)
+            {
+                Console.Write(b + " ");
+                Console.Write(a + " ");
+            }
+            else
+            {
+                Console.Write(a + " ");
+                Console.Write(b + " ");
+            }
+
+        }
+        Console.WriteLine();
     }
 }
 
