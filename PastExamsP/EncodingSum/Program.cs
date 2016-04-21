d9e27fdeb157885bf5203588cb76941a9f1a2786
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-04-21 22:33:22 +0300

    added more past exams

---
+++ PastExamsP/EncodingSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int module = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        int result = 0;
+        text = text.ToLower();
+        for (int i = 0; i < text.Length; i++)
+        {
+            char ch = text[i];
+            if (ch == '@')
+            {
+                break;
+            }
+            else if (ch >= '0' && ch <= '9')
+            {
+                result *= ch - '0';
+            }
+            else if (ch >= 'a' && ch <= 'z')
+            {
+                result += ch - 'a';
+            }
+            else
+            {
+                result %= module;
+            }
+
+        }
+        Console.WriteLine(result);
+    }
+}
+
