3cd59cb19ed7d0b446a36eaf3d7ecc09271a2a60
Kalito98 <Kaloyan.Yanev98@gmail.com>
2016-04-21 17:33:33 +0300

    Added past exams

---
+++ PastExamsP/Cube3D/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char space = ' ';
+        char vertical = '|';
+        char horizontal = '-';
+        char sides = ':';
+        int workSpace = n * 2 - 1;
+
+        for (int col = 0; col < workSpace; col++)
+        {
+            Console.WriteLine(sides);
+            for (int row = 0; row < workSpace; row++)
+            {
+                if (col == 0 && row >= n)
+                {
+                    Console.WriteLine(sides);
+                }
+            }
+        }
+
+    }
+}
+
