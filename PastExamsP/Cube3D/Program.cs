580832802bb9629b659e3948f38e2107c9f980af
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-04-21 22:11:59 +0300

    Fuck this shit / past exams

--- PastExamsP/Cube3D/Program.cs
+++ PastExamsP/Cube3D/Program.cs
@@ -10,17 +10,44 @@ class Program
         char horizontal = '-';
         char sides = ':';
         int workSpace = n * 2 - 1;
+        int mid = n - 1;
 
-        for (int col = 0; col < workSpace; col++)
+        for (int row = 0; row < workSpace; row++)
         {
-            Console.WriteLine(sides);
-            for (int row = 0; row < workSpace; row++)
+            for (int col = 0; col < workSpace; col++)
             {
-                if (col == 0 && row >= n)
+                if (row == 0 && col < n ||
+                    row == mid && col < n ||
+                    col == 0 && row < n ||
+                    col == mid && row < n ||
+                    col >= mid && row == workSpace - 1 ||
+                    col == workSpace - 1 && row >= mid ||
+                    col - row == mid ||
+                    col - row == 0 && col > mid ||
+                    col - row == n - workSpace
+
+                    )
                 {
-                    Console.WriteLine(sides);
+                    Console.Write(sides);
+                }
+                else if (
+                    col > mid && col - row <= mid && col - row >= 0
+                    )
+                {
+                    Console.Write(vertical);
+                }
+                else if (
+                    row > mid && col - row >= n - workSpace
+                    )
+                {
+                    Console.Write(horizontal);
+                }
+                else
+                {
+                    Console.Write(space);
                 }
             }
+            Console.WriteLine();
         }
 
     }
---
+++ PastExamsP/PersianRugs/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sizeBig =  int.Parse(Console.ReadLine());
+        int sizeSmall = int.Parse(Console.ReadLine());
+        int workSpace = sizeBig * 2 + 1;
+        int mid = sizeBig + 1;
+        char sideLeft = '/';
+        char sideRight = '\\';
+        char colorBig = '#';
+        char colorSmall = '.';
+        char space = ' ';
+
+        for (int row = 0; row < workSpace; row++)
+        {
+            for (int col = 0; col < workSpace; col++)
+            {
+                if (
+                    col == sizeBig && row == sizeBig
+                    )
+                {
+                    Console.Write('X');
+                }
+                else if 
+                    (
+                    col - row == 0 ||
+                    col - row == sizeSmall + 1 && row < 2 ||
+                    col - row == sizeSmall - sizeBig && row > workSpace - 3
+                    )
+                {
+                    Console.Write(sideRight);
+                }
+                else if
+                    (
+                    col + row == workSpace - 1 || 
+                    col + row == workSpace - 1 - sizeSmall - 1 && row < 2 ||
+                    col + row == workSpace + sizeSmall && row > workSpace - 3
+                    )
+                {
+                    Console.Write(sideLeft);
+                }
+                else if 
+                    (
+                    col > sizeBig && col < sizeBig ||
+                    col - row <= 0 && col + row <= workSpace - 1 ||
+                    col - row >= 0 && col + row >= workSpace - 1
+                    )
+                {
+                    Console.Write(colorBig);
+                }
+               else if 
+                   (
+                   row == 0 && col > row + sizeSmall && col < row + sizeSmall + sizeBig ||
+                   row == 1 && col == sizeBig ||
+                   row == workSpace - 1 && col > row - sizeBig - sizeSmall && col < row - sizeSmall ||
+                   row == workSpace - 2 && col == sizeBig
+                   )
+               {
+                   Console.Write(colorSmall);
+               }
+                else
+                {
+                    Console.Write(space);
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+           
+    }
+}
+
