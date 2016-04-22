1c26e54ced7a25ce0b4d01745f22851ce86d5d9a
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-04-22 22:01:47 +0300

    Added some past exams and some homework

---
+++ Loops/MMSAofNNumbers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double min = 10000000000;
+        double max = 0;
+        double avg = 0;
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int currN = int.Parse(Console.ReadLine());
+            min = Math.Min(currN, min);
+            max = Math.Max(currN, max);
+            sum += currN;
+            avg += currN;
+        }
+        avg = avg / n;
+        Console.Write("min=");
+        Console.WriteLine("{0:F2}", min);
+        Console.Write("max=");
+        Console.WriteLine("{0:F2}", max);
+        Console.Write("sum=");
+        Console.WriteLine("{0:F2}", sum);
+        Console.Write("avg=");
+        Console.WriteLine("{0:F2}", avg);
+
+    }
+}
+
+//using System;
+//
+//    class MMSAofNNumbers
+//{
+//    static void Main()
+//    {
+//        int n = int.Parse(Console.ReadLine());
+//        double number1 = double.Parse(Console.ReadLine());
+//        double min = number1;
+//        double max = number1;
+//        double sum = number1;
+//        double avg = number1;
+//        for (int i = 2; i <= n; i++)
+//        {
+//            double number = double.Parse(Console.ReadLine());
+//            min = Math.Min(min, number);
+//            max = Math.Max(max, number);
+//            sum = sum + number;
+//            avg = sum / i;
+//        }
+//        Console.WriteLine("min={0:F2}", min);
+//        Console.WriteLine("max={0:F2}", max);
+//        Console.WriteLine("sum={0:F2}", sum);
+//        Console.WriteLine("avg={0:F2}", avg);
+//    }
+//}
+//
---
+++ PastExamsP/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int counter = 1;
+        long curResult = 1;
+        long result = 1;
+        string text = "";
+        while (true)
+        {
+            if (counter % 2 == 0)
+            {
+                text = Console.ReadLine();
+                if (text == "END")
+                {
+                    break;
+                }
+                long number = int.Parse(text);
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    long lastNumber = number % 10;
+                    curResult = curResult * lastNumber;
+                }
+                result *= curResult;
+                curResult = 1;
+            }
+            counter++;
+        }
+        Console.WriteLine(result);
+    }
+}
+
